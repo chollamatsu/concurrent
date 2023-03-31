2e6b4cbb7090262046d9195924d0d7f40f819e96
chollamatsu <chonlamas.ch@gmail.com>
2023-03-31 15:26:50 +0700

    assign1: say "Hello World"

---
+++ HW1.go
@@ -0,0 +1,20 @@
+// main sai "hello" goroutine say "world"
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func pong(wg *sync.WaitGroup) {
+	fmt.Println("goroutine: world")
+	wg.Done()
+}
+func main() {
+	fmt.Println("main: hello")
+	wg.Add(1)
+	go pong(&wg)
+	wg.Wait()
+}

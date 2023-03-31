dac65b713c6e34bb69c2e0ed346e77ea8d90a454
chollamatsu <chonlamas.ch@gmail.com>
2023-03-31 15:22:22 +0700

    assignment1: say "hello world"

---
+++ HW1_1_620510591.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func pong(wg *sync.WaitGroup) {
+	fmt.Println("goroutine: world")
+	wg.Done()
+}
+func main() {
+	fmt.Println("main: hello")
+	wg.Add(1)
+	go pong(&wg)
+	wg.Wait()
+}
